e21d7003938bd39e2d59990fed996f9cadc1d4c8
sage_koala <brandondukescodes@gmail.com>
2024-03-02 17:20:57 -0600

    Add functions to play a game, verify correct input, and display the results of computer v. user guess

--- wordle.py
+++ wordle.py
@@ -5,7 +5,7 @@ import sys
 
 ## Add constants to modify color of terminal stdout
 WRONG = "\033[31m"
-CLOSE = "\033[103m"
+CLOSE = "\033[33m"
 CORRECT = "\033[32m"
 RESET = "\033[39m"
 
@@ -25,11 +25,15 @@ def main():
     if wordsize != 5 and wordsize != 6:
         sys.exit("Argument needs to be either the number 5 or 6")
     
-    print(get_current_game_word(wordsize))
+    # Get words
+    current_game_word = get_current_game_word(wordsize)
 
-    print(get_user_guess(wordsize))
+    # Send current words to play round function
+    rounds = wordsize + 1
+    result = play_game(current_game_word, rounds, wordsize)
 
-    print(WRONG+"BRAND"+RESET)
+    print(result)
+
 
 def get_current_game_word(wordsize):
     """Reads in list of 5 or 6 letter words based on user's wordsize argument
@@ -45,16 +49,19 @@ def get_current_game_word(wordsize):
 
     return current_game_word
 
-def get_user_guess(wordsize):
+def get_user_guess(wordsize, rounds):
 
     user_guess = ""
 
-    while len(user_guess) != wordsize or not check_correct_guess_format(user_guess):
-        user_guess = input(f"Enter a {wordsize}-letter guess: ")
+    while not check_correct_guess_format(user_guess, wordsize):
+        user_guess = input(f"{rounds} guesses remaining - Enter a {wordsize}-letter guess: ").lower()
 
     return user_guess
 
-def check_correct_guess_format(guess):
+def check_correct_guess_format(guess, wordsize):
+
+    if len(guess) != wordsize:
+        return False
 
     for letter in guess:
         if not letter.isalpha():
@@ -62,17 +69,43 @@ def check_correct_guess_format(guess):
         
     return True
 
+def display_guess_correctness(guess, current_game_word):
 
+    if guess == current_game_word:
+        print(CORRECT+guess+RESET)
+        return True
 
+    for i, letter in enumerate(guess):
+        if letter == current_game_word[i]:
+            print(CORRECT+letter+RESET, end="")
+        elif letter_in_word(letter, current_game_word):
+            print(CLOSE+letter+RESET, end="")
+        else:
+            print(WRONG+letter+RESET, end="")
 
+    print()
 
+    return False
 
+def letter_in_word(guess_letter, current_game_word):
 
+    for c in current_game_word:
+        if c == guess_letter:
+            return True
+        
+    return False
 
+def play_game(current_game_word, rounds, wordsize):
 
+    while rounds > 0:
+        user_guess = get_user_guess(wordsize, rounds)
+        correct = display_guess_correctness(user_guess, current_game_word)
+        
+        if correct:
+            return f"You won! The correct guess was {current_game_word}"
+        rounds -= 1
 
-
-
+    return f"You ran out of guesses! The correct guess was {current_game_word}"
 
 if __name__ == "__main__":
     main()
